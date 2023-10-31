2663ec873e04f318d9e94d34cb370a0d003fe90f
h-rub <rubio@loopgk.com>
2023-10-31 15:22:23 -0600

    CORS_ORIGIN_ALLOW_ALL

--- core/core/settings.py
+++ core/core/settings.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://em10vs0010.embraco.com:9001",
